06199f23dc1af0a0325fe27dc16e98b45dadae90
Kelsie Braidwood <kelsieiona@gmail.com>
2018-04-13 08:29:22 +0100

    CRUD functions added for customer class

---
+++ models/customer.rb
@@ -0,0 +1,47 @@
+require_relative("../db/sql_runner")
+require_relative("film.rb")
+
+class customer_id
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize( options )
+    @id = options['id'].to_i
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name) VALUES ($1, $2) RETURNING id"
+    values = [@name, @funds]
+    user = SqlRunner.run( sql, values ).first
+    @id = user['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    values = []
+    customers = SqlRunner.run(sql, values)
+    result = User.map_customers(customers)
+    return result
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM customers"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def films()
+    sql = "SELECT films.* FROM films INNER JOIN tickets ON films.id = tickets.film_id WHERE tickers.customer_id = $1"
+    values = [@id]
+    films = SqlRunner.run(sql, values)
+    return Film.map_films(films)
+  end
+
+  def self.map_customers(customer_data)
+    return customer_data.map{|customer| Customer.new(customer)}
+  end
+
+end
